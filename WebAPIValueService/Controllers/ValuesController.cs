b01f1137ec3aa684ca99974fa117665f214a57b3
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2020-07-10 22:14:29 +0530

    ValuesController Added

---
+++ WebAPIValueService/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIValueService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value a", "value b" };
+        }
+    }
+}
